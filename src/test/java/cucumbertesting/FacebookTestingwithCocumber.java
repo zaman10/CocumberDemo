1708a449eb1585d4d482d582919147d790075872
Zaman <muhammadzaman_zaman@hotmail.com>
2021-10-14 12:05:22 +0500

    First change

---
+++ src/test/java/cucumbertesting/FacebookTestingwithCocumber.java
@@ -0,0 +1,26 @@
+package cucumbertesting;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class FacebookTestingwithCocumber {
+	@When("^I enetr username as \"([^\"]*)\"$")
+	public void i_enetr_username_as(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enetr password as \"([^\"]*)\"$")
+	public void i_enetr_password_as(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Login should fail$")
+	public void login_should_fail() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
---
+++ target/test-classes/login.feature
@@ -0,0 +1,6 @@
+Feature: Facebook login page testing
+
+  Scenario: 
+    When I enetr username as "Zaman"
+    And I enetr password as "12345678"
+    Then Login should fail
